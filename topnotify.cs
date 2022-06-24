64c44da4a214c2e8c282faee4ca7fa57fa306375
APerson <50999514+eRandomPerson@users.noreply.github.com>
2022-06-24 14:15:12 +0700

    Update the position
    More MyDockFinder friendly

--- topnotify.cs
+++ topnotify.cs
@@ -34,7 +34,7 @@ public class Program {
             if (System.AppDomain.CurrentDomain.FriendlyName == "topleft.exe"){
                 //Sets to top left (easy peasy)
                 //50PX Y offset to make the spacing even
-                SetWindowPos(hwnd, 0, 0, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                SetWindowPos(hwnd, 0, 0, -37, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
             }
             else if (System.AppDomain.CurrentDomain.FriendlyName == "topright.exe"){
                 //Sets to top left (not as easy)
@@ -47,7 +47,7 @@ public class Program {
 			    NotifyRect.Height = NotifyRect.Height - NotifyRect.Y;
 
                 //50PX Y offset to make the spacing even
-                SetWindowPos(hwnd, 0, Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width, -50, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
+                SetWindowPos(hwnd, 0, Screen.PrimaryScreen.Bounds.Width - NotifyRect.Width, -37, 0, 0, SWP_NOSIZE | SWP_NOZORDER | SWP_SHOWWINDOW);
             }
 
             Thread.Sleep(10);
